33c943e258876f2d14efbd0ac72c06fb6df388fc
Pápai Attila <Ppai Attila>
2016-04-06 09:55:27 +0200

    skeleton

---
+++ Skeleton.cpp
@@ -0,0 +1,310 @@
+//=============================================================================================
+// Szamitogepes grafika hazi feladat keret. Ervenyes 2016-tol.
+// A //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// sorokon beluli reszben celszeru garazdalkodni, mert a tobbit ugyis toroljuk.
+// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat.
+// Tilos:
+// - mast "beincludolni", illetve mas konyvtarat hasznalni
+// - faljmuveleteket vegezni a printf-et kivéve
+// - new operatort hivni a lefoglalt adat korrekt felszabaditasa nelkul
+// - felesleges programsorokat a beadott programban hagyni
+// - felesleges kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
+// ---------------------------------------------------------------------------------------------
+// A feladatot ANSI C++ nyelvu forditoprogrammal ellenorizzuk, a Visual Studio-hoz kepesti elteresekrol
+// es a leggyakoribb hibakrol (pl. ideiglenes objektumot nem lehet referencia tipusnak ertekul adni)
+// a hazibeado portal ad egy osszefoglalot.
+// ---------------------------------------------------------------------------------------------
+// A feladatmegoldasokban csak olyan OpenGL fuggvenyek hasznalhatok, amelyek az oran a feladatkiadasig elhangzottak 
+//
+// NYILATKOZAT
+// ---------------------------------------------------------------------------------------------
+// Nev    : 
+// Neptun : 
+// ---------------------------------------------------------------------------------------------
+// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
+// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
+// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a
+// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb
+// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem,
+// azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
+// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.
+// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
+// negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
+//=============================================================================================
+
+#define _USE_MATH_DEFINES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#if defined(__APPLE__)
+#include <GLUT/GLUT.h>
+#include <OpenGL/gl3.h>
+#include <OpenGL/glu.h>
+#else
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#include <windows.h>
+#endif
+#include <GL/glew.h>		// must be downloaded 
+#include <GL/freeglut.h>	// must be downloaded unless you have an Apple
+#endif
+
+const unsigned int windowWidth = 600, windowHeight = 600;
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Innentol modosithatod...
+
+// OpenGL major and minor versions
+int majorVersion = 3, minorVersion = 0;
+
+void getErrorInfo(unsigned int handle) {
+	int logLen;
+	glGetShaderiv(handle, GL_INFO_LOG_LENGTH, &logLen);
+	if (logLen > 0) {
+		char * log = new char[logLen];
+		int written;
+		glGetShaderInfoLog(handle, logLen, &written, log);
+		printf("Shader log:\n%s", log);
+		delete log;
+	}
+}
+
+// check if shader could be compiled
+void checkShader(unsigned int shader, char * message) {
+	int OK;
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &OK);
+	if (!OK) {
+		printf("%s!\n", message);
+		getErrorInfo(shader);
+	}
+}
+
+// check if shader could be linked
+void checkLinking(unsigned int program) {
+	int OK;
+	glGetProgramiv(program, GL_LINK_STATUS, &OK);
+	if (!OK) {
+		printf("Failed to link shader program!\n");
+		getErrorInfo(program);
+	}
+}
+
+// vertex shader in GLSL
+const char *vertexSource = R"(
+	#version 130
+    precision highp float;
+
+	in vec2 vertexPosition;		// variable input from Attrib Array selected by glBindAttribLocation
+	out vec2 texcoord;			// output attribute: texture coordinate
+
+	void main() {
+		texcoord = (vertexPosition + vec2(1, 1))/2;							// -1,1 to 0,1
+		gl_Position = vec4(vertexPosition.x, vertexPosition.y, 0, 1); 		// transform to clipping space
+	}
+)";
+
+// fragment shader in GLSL
+const char *fragmentSource = R"(
+	#version 130
+    precision highp float;
+
+	uniform sampler2D textureUnit;
+	in  vec2 texcoord;			// interpolated texture coordinates
+	out vec4 fragmentColor;		// output that goes to the raster memory as told by glBindFragDataLocation
+
+	void main() {
+		fragmentColor = texture(textureUnit, texcoord); 
+	}
+)";
+
+
+struct vec4 {
+	float v[4];
+
+	vec4(float x = 0, float y = 0, float z = 0, float w = 1) {
+		v[0] = x; v[1] = y; v[2] = z; v[3] = w;
+	}
+};
+
+
+// handle of the shader program
+unsigned int shaderProgram;
+
+class FullScreenTexturedQuad {
+	unsigned int vao, textureId;	// vertex array object id and texture id
+public:
+	void Create(vec4 image[windowWidth * windowHeight]) {
+		glGenVertexArrays(1, &vao);	// create 1 vertex array object
+		glBindVertexArray(vao);		// make it active
+
+		unsigned int vbo;		// vertex buffer objects
+		glGenBuffers(1, &vbo);	// Generate 1 vertex buffer objects
+
+		// vertex coordinates: vbo[0] -> Attrib Array 0 -> vertexPosition of the vertex shader
+		glBindBuffer(GL_ARRAY_BUFFER, vbo); // make it active, it is an array
+		static float vertexCoords[] = { -1, -1,   1, -1,  -1, 1, 
+			                             1, -1,   1,  1,  -1, 1 };	// two triangles forming a quad
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertexCoords), vertexCoords, GL_STATIC_DRAW);	   // copy to that part of the memory which is not modified 
+		// Map Attribute Array 0 to the current bound vertex buffer (vbo[0])
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,	0, NULL);     // stride and offset: it is tightly packed
+
+		// Create objects by setting up their vertex data on the GPU
+		glGenTextures(1, &textureId);  				// id generation
+		glBindTexture(GL_TEXTURE_2D, textureId);    // binding
+
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, windowWidth, windowHeight, 0, GL_RGBA, GL_FLOAT, image); // To GPU
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); // sampling
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	}
+
+	void Draw() {
+		glBindVertexArray(vao);	// make the vao and its vbos active playing the role of the data source
+		int location = glGetUniformLocation(shaderProgram, "textureUnit");
+		if (location >= 0) {
+			glUniform1i(location, 0);		// texture sampling unit is TEXTURE0
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, textureId);	// connect the texture to the sampler
+		}
+		glDrawArrays(GL_TRIANGLES, 0, 6);	// draw two triangles forming a quad
+	}
+};
+
+// The virtual world: single quad
+FullScreenTexturedQuad fullScreenTexturedQuad;
+
+// Initialization, create an OpenGL context
+void onInitialization() {
+	glViewport(0, 0, windowWidth, windowHeight);
+
+	static vec4 background[windowWidth * windowHeight];
+	for (int x = 0; x < windowWidth; x++) {
+		for (int y = 0; y < windowHeight; y++) {
+			background[y * windowWidth + x] = vec4((float)x / windowWidth, (float)y / windowHeight, 0, 1);
+		}
+	}
+	fullScreenTexturedQuad.Create( background );
+
+	// Create vertex shader from string
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	if (!vertexShader) {
+		printf("Error in vertex shader creation\n");
+		exit(1);
+	}
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	checkShader(vertexShader, "Vertex shader error");
+
+	// Create fragment shader from string
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	if (!fragmentShader) {
+		printf("Error in fragment shader creation\n");
+		exit(1);
+	}
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	checkShader(fragmentShader, "Fragment shader error");
+
+	// Attach shaders to a single program
+	shaderProgram = glCreateProgram();
+	if (!shaderProgram) {
+		printf("Error in shader program creation\n");
+		exit(1);
+	}
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+
+	// Connect Attrib Arrays to input variables of the vertex shader
+	glBindAttribLocation(shaderProgram, 0, "vertexPosition"); // vertexPosition gets values from Attrib Array 0
+
+	// Connect the fragmentColor to the frame buffer memory
+	glBindFragDataLocation(shaderProgram, 0, "fragmentColor");	// fragmentColor goes to the frame buffer memory
+
+	// program packaging
+	glLinkProgram(shaderProgram);
+	checkLinking(shaderProgram);
+	// make this program run
+	glUseProgram(shaderProgram);
+}
+
+void onExit() {
+	glDeleteProgram(shaderProgram);
+	printf("exit");
+}
+
+// Window has become invalid: Redraw
+void onDisplay() {
+	glClearColor(0, 0, 0, 0);							// background color 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the screen
+	fullScreenTexturedQuad.Draw();
+	glutSwapBuffers();									// exchange the two buffers
+}
+
+// Key of ASCII code pressed
+void onKeyboard(unsigned char key, int pX, int pY) {
+	if (key == 'd') glutPostRedisplay();         // if d, invalidate display, i.e. redraw
+}
+
+// Key of ASCII code released
+void onKeyboardUp(unsigned char key, int pX, int pY) {
+
+}
+
+// Mouse click event
+void onMouse(int button, int state, int pX, int pY) {
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {  // GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON and GLUT_DOWN / GLUT_UP
+	}
+}
+
+// Move mouse with key pressed
+void onMouseMotion(int pX, int pY) {
+}
+
+// Idle event indicating that some time elapsed: do animation here
+void onIdle() {
+	long time = glutGet(GLUT_ELAPSED_TIME); // elapsed time since the start of the program
+}
+
+// Idaig modosithatod...
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int main(int argc, char * argv[]) {
+	glutInit(&argc, argv);
+#if !defined(__APPLE__)
+	glutInitContextVersion(majorVersion, minorVersion);
+#endif
+	glutInitWindowSize(windowWidth, windowHeight);				// Application window is initially of resolution 600x600
+	glutInitWindowPosition(100, 100);							// Relative location of the application window
+#if defined(__APPLE__)
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_3_2_CORE_PROFILE);  // 8 bit R,G,B,A + double buffer + depth buffer
+#else
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+#endif
+	glutCreateWindow(argv[0]);
+
+#if !defined(__APPLE__)
+	glewExperimental = true;	// magic
+	glewInit();
+#endif
+
+	printf("GL Vendor    : %s\n", glGetString(GL_VENDOR));
+	printf("GL Renderer  : %s\n", glGetString(GL_RENDERER));
+	printf("GL Version (string)  : %s\n", glGetString(GL_VERSION));
+	glGetIntegerv(GL_MAJOR_VERSION, &majorVersion);
+	glGetIntegerv(GL_MINOR_VERSION, &minorVersion);
+	printf("GL Version (integer) : %d.%d\n", majorVersion, minorVersion);
+	printf("GLSL Version : %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+	onInitialization();
+
+	glutDisplayFunc(onDisplay);                // Register event handlers
+	glutMouseFunc(onMouse);
+	glutIdleFunc(onIdle);
+	glutKeyboardFunc(onKeyboard);
+	glutKeyboardUpFunc(onKeyboardUp);
+	glutMotionFunc(onMouseMotion);
+
+	glutMainLoop();
+	onExit();
+	return 1;
+}
